df6ca98aa3f04e11dc6abf89b7c7c5db8c9f0a1a
baliankit24 <40552349+baliankit24@users.noreply.github.com>
2018-06-28 16:28:31 +0530

    Add files via upload

---
+++ Libraries/ContactManager.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using ContactManger.DAL.Repository;
+using System;
+namespace ContactManger.DAL.UnitOfWork
+    
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        #region Properties
+
+        IGenericRepository<Contact> ContactRepository { get; }
+        
+        #endregion
+
+        #region Public methods
+        
+        /// <summary>
+        /// Save all the changes of the context to the database.
+        /// </summary>
+        void Commit();
+
+        #endregion
+    }
+}
